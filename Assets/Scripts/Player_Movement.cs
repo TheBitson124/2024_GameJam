e76dd59f75091b9fbffe0929dc5bf8c0d3ce3c78
s27502 <s27502@pjwstk.edu.pl>
2024-11-30 15:09:27 +0100

    player movement epic

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float horizontal;
+    private float speed = 7f;
+    private bool isFacingRight = true;
+    private bool isGravityReversed = false;
 
-    // Update is called once per frame
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private Transform _groundCheckUp;
+    [SerializeField] private Transform _groundCheckDown;
+    [SerializeField] private LayerMask _groundLayer;
+    
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            if (_rb.gravityScale > 0)
+            {
+                isGravityReversed = true;
+                _rb.gravityScale = -4;
+            }
+            else
+            {
+                isGravityReversed = false;
+                _rb.gravityScale = 4;
+            }
+        }
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(horizontal * speed, _rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        if (isGravityReversed)
+        {
+            return Physics2D.OverlapCircle(_groundCheckUp.position, 0.2f, _groundLayer);
+        }
+        else
+        {
+            return Physics2D.OverlapCircle(_groundCheckDown.position, 0.2f, _groundLayer);
+        }
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
