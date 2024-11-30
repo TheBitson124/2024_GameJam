9c31e956a933e270d1dc87e5842f2fdb375cd78f
s27466 <161813484+s27466@users.noreply.github.com>
2024-11-30 14:57:49 +0100

    poprawka pizzy, gwiazd i dodanie spike

--- Assets/Scripts/Star_Script.cs
+++ Assets/Scripts/Star_Script.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Star_Script : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
