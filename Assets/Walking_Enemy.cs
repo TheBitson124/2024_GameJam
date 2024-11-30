c0145144b4f468e7990f3697d2838341d40c4966
s27466 <161813484+s27466@users.noreply.github.com>
2024-11-30 19:57:53 +0100

    springy anim

--- Assets/Walking_Enemy.cs
+++ Assets/Walking_Enemy.cs
@@ -8,14 +8,19 @@ public class Walking_Enemy : Enemy_Script
     [SerializeField] private float Speed;
     private SpriteRenderer Renderer;
     private float Counter = 0;
+    private Animator _animator;
+    private float horizontal;
 
     private void Awake()
     {
+        _animator = GetComponent<Animator>();
         Renderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        _animator.SetInteger("Speed", Math.Abs((int)horizontal));
         if (!other.CompareTag("Player")) { return; }
         var multi = other.gameObject.transform.position.x > transform.position.x ? 1 : -1;
         transform.position += new Vector3(multi * Speed * Time.deltaTime, 0, 0);
