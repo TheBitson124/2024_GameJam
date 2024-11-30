f593e257159c2982391593b0902eacc4ec9cdb29
s27502 <s27502@pjwstk.edu.pl>
2024-11-30 14:34:35 +0100

    movement w.i.p.

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float horizontal;
+    private float speed = 5f;
+    private float jumpPower = 16;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+    
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            //Tu grawitacja lolololo
+        }
         
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(horizontal * speed, _rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
