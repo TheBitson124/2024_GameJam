9e4737e1baf046c4bca00d47233144324a754229
s27502 <s27502@pjwstk.edu.pl>
2024-11-30 16:27:28 +0100

    aaa

--- Assets/Projectile_Script.cs
+++ Assets/Projectile_Script.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,17 @@ using UnityEngine;
 public class Projectile_Script : MonoBehaviour
 {
     [SerializeField] private float Damage;
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private int speed;
+    private void Awake()
+    {
+        _rb.velocity = new Vector2(speed, _rb.velocity.y);
+        StartCoroutine(ProjectileDestroy());
+    }
+    
+    private IEnumerator ProjectileDestroy()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
 }
